601b6419b42edd1a0bd46df498f52119df838bae
Jacob Schmidt <schmidtj4779@my.uwstout.edu>
2025-02-25 13:00:17 -0600

    Defined highlight and blank and related methods

--- src/KoiKoi_Display.cpp
+++ src/KoiKoi_Display.cpp
@@ -1,1 +1,21 @@
+#include "KoiKoi_Display.h"
+#include <raylib.h>
 
+Texture2D highlight_;
+Texture2D back_;
+
+void loadHighlight() {
+	highlight_ = LoadTexture("Hanafuda Cards/highlight.png");
+};
+
+void unloadHighlight() {
+	UnloadTexture(highlight_);
+};
+
+void loadBack() {
+	back_ = LoadTexture("Hanafuda Cards/backCard.png");
+};
+
+void unloadBack() {
+	UnloadTexture(back_);
+};
